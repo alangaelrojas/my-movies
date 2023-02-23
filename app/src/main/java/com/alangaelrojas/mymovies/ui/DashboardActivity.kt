3b18c1d7849e9d4aa9647f2738fa31d3d1e712b4
alangaelrojas <alangaelrojas@gmail.com>
2023-02-22 20:29:28 -0600

    IntentsTipos de variables en Kotlin
    Vista de Dashboard de peliculas

---
+++ app/src/main/java/com/alangaelrojas/mymovies/ui/DashboardActivity.kt
@@ -0,0 +1,108 @@
+package com.alangaelrojas.mymovies.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.Toast
+import com.alangaelrojas.mymovies.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+
+class DashboardActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+
+    // Variable declarada con valor inicial
+    private var a: Int = 0
+    private var b: Int = 0
+    private var c: Int = 0
+
+    // Variable con valor nulo
+    private var aa: Int? = null
+    private var ab: Int? = null
+    private var ac: Int? = null
+
+    // Variable con inicializacion tardia
+    private lateinit var ba: String
+    private lateinit var bb: String
+    private lateinit var bc: String
+
+    // Variable con inicializacion peresoza
+    private val ca: Int by lazy { 0 }
+    private val cb by lazy { "Hola" }
+    private val cc: Boolean by lazy { true }
+
+    // Widgets o views
+    private lateinit var imgUser: ImageView
+    private lateinit var container: FrameLayout
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        // Assign to ours views values
+        imgUser = findViewById(R.id.imgUser)
+        container = findViewById(R.id.container)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        val greeting = intent.getStringExtra("greeting")
+        Toast.makeText(this, greeting, Toast.LENGTH_SHORT).show()
+
+
+
+        imgUser.setOnClickListener {
+            //  Aqui vamos a escuchar cuando se de click en imgUser
+
+        }
+
+        bottomNavigationView.setOnItemSelectedListener(this)
+
+
+
+    }
+
+    private fun withLazyInit() {
+        //Peresoza
+        if (cc){ }
+        val number: Int = ca
+        val greeting: String = cb
+    }
+
+    private fun withLateInit() {
+        Toast.makeText(this, ba, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun withNulls() {
+        aa?.let { aaAsNotNull ->
+            val sum: Int = aaAsNotNull + 10
+        }
+
+        val substraction = ab!! - 20
+
+    }
+
+    private fun withValues() {
+        if (a != null){
+            val sum = a + 10
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.navMovies -> {
+                Toast.makeText(this, "Movies", Toast.LENGTH_SHORT).show()
+            }
+            R.id.navSearch -> {
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
+            }
+            R.id.navDiscover -> {
+                Toast.makeText(this, "Movies", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return true
+    }
+}
